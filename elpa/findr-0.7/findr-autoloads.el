d2a8563542f2fad4377d6a709569000d07bc6899
Jai Keerthi <haiiamjai@gmail.com>
2011-01-13 15:47:44 +0530

    added elpa packages and color-theme packages

---
+++ elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,18 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19566 22446
+;;;;;;  866210))
+
+;;;***
+
+(provide 'findr-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; findr-autoloads.el ends here
